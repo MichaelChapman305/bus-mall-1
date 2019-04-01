b53245904c0c8432c2f65d99cf5763b430b9a689
Jeff Lawrence <jefflaw13@hotmail.com>
2019-04-01 16:55:45 -0700

    end of class progress

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
+'use strict';
+
+
 var container = document.getElementById('container');
+var totalClicks = 0;
+
+
 
 function handleClick(event) {
+  if(event.target.className === 'product'){
+    // event.target.id
+    totalClicks++;
 
+  }
 }
 
+container.addEventListener('click', handleClick);
