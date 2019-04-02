6ae88ac767b96c4d2ff72444baabc2111cfcc3a1
Jeff Lawrence <jefflaw13@hotmail.com>
2019-04-01 21:14:07 -0700

    removed product js/ moved producArray to app.js/ started mathrandom

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 var container = document.getElementById('container');
 var totalVotesOnPage = 0;
 var PRODUCTS = {};
+var lastProducts = [];
+var currentProducts = [];
 
 function Product(name, HTMLid, imgURL){
   this.name = name;
@@ -30,7 +32,7 @@ Product.prototype.render = function(parentId){
 };
 
 function randomImageSelector(){
-
+  var randomNum = Math.floor((Math.random() * productArray.length) + 1);
 }
 
 function addCurrentImages(event){
@@ -46,6 +48,7 @@ function handleClick(event) {
     totalVotesOnPage++;
     PRODUCTS[event.target.id].totalVotes++;
     //TODO if total clicks stop listening
+    console.log(productArray);
     if(totalVotesOnPage === 25){
       //TODO remove eventlistener from container
       displayResults();
@@ -58,3 +61,27 @@ function handleClick(event) {
 }
 
 container.addEventListener('click', handleClick);
+
+
+var productArray = [
+  ['Bag', 'bag', './img/bag.jpg'],
+  ['Banana', 'banana', './img/banana.jpg'],
+  ['Bathroom', 'bathroom', './img/bathroom.jpg'],
+  ['Boots', 'boots', './img/boots.jpg'],
+  ['Breakfast', 'breakfast', './img/breakfast.jpg'],
+  ['Bubblegum', 'bubblegum', './img/bubblegum.jpg'],
+  ['Chair', 'chair', './img/chair.jpg'],
+  ['Cthulu', 'cthulu', './img/cthulu.jpg'],
+  ['Dog and Duck', 'dogDuck', './img/dog-duck.jpg'],
+  ['Dragon', 'dragon', './img/dragon.jpg'],
+  ['Pen', 'pen', './img/pen.jpg'],
+  ['Pet Sweep', 'petSweep', './img/pet-sweep.jpg'],
+  ['Scissors', 'scissors', './img/scissors.jpg'],
+  ['Shark', 'shark', './img/shark.jpg'],
+  ['Sweep', 'sweep', './img/sweep.png'],
+  ['Tauntaun', 'tauntaun', './img/tauntaun.jpg'],
+  ['Unicorn', 'unicorn', './img/unicorn.jpg'],
+  ['Usb', 'usb', './img/usb.gif'],
+  ['Water Can', 'waterCan', './img/water-can.jpg'],
+  ['Wine Glass', 'wineGlass', './img/win-glass.jpg'],
+];
