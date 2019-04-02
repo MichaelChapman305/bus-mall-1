f6d1cb5c07f401d3d1f5b6bda74a533276f7c59c
Jeff Lawrence <jefflaw13@hotmail.com>
2019-04-01 20:18:49 -0700

    added constructor, worked a bit on eventhandler, a bit on render

--- js/app.js
+++ js/app.js
@@ -3,13 +3,36 @@
 
 var container = document.getElementById('container');
 var totalClicks = 0;
+var productArray = [];
 
+function Product(imgURL, name, HTMLid){
+  this.imgURL = imgURL;
+  this.name = name;
+  this.totalVotes = this.totalViews = 0;
+  this.HTMLid = HTMLid;
 
+  productArray.push(this);
+}
+
+Product.prototype.clickPercent = function(){
+  return this.totalViews / this.totalViews;
+};
+
+Product.prototype.render = function(parentId){
+  var parent = document.getElementById(parentId);
+
+  var img = document.createElement('img');
+  img.setAttribute('id', this.HTMLid);
+  img.setAttribute('src', this.imgURL);
+  img.setAttribute('class', 'product');
+
+  parent.appendChild(img);
+};
 
 function handleClick(event) {
   if(event.target.className === 'product'){
-    // event.target.id
-    totalClicks++;
+    console.log(event.target.className);
+        // totalClicks++;
 
   }
 }
